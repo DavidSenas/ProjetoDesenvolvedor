354db9ce84ac119ca07d8bb06287b6e3b8312d2e
DavidSenas <dvd.senas@outlook.com>
2021-07-12 21:07:05 -0300

    Realizado alguns ajustes e limpeza de código apenas para criação do banco.

--- conectando_criando.py
+++ conectando_criando.py
@@ -2,16 +2,7 @@ import sqlite3
 try:
     bd = sqlite3.connect("banco_dados")
     cursor = bd.cursor()
-    cursor.execute("CREATE TABLE locais(cep integer, end text, bairro text, estado text)")
-    bd.commit()
-    cursor.execute('SELECT * FROM LOCAIS')
-    for c in cursor.fetchall():
-        print("===" * 20)
-        for x in c:
-            print(x)
 except:
     print("Erro na criação do banco")
 else:
     print("Banco criado com sucesso")
-
- 
